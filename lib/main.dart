7a95f24d32ef9ebf1ac6eb3d185f1ba0aad52783
CABREX <asvamogh@gmail.com>
2022-02-04 23:56:02 +0530

    Added Signup Page and reworked design

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,14 @@
 // import 'dart:html';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+// Global Variables
+var _username = ["username"];
+var _password = ["password123"];
+int _i = 1;
 
 void main() {
   runApp(const MyApp());
@@ -63,10 +70,20 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _isObscure = true;
   bool _isVisible = false;
 
-  fetchCredentials() {
-    var username = "username";
-    var password = "password123";
-    return [username, password];
+  final TapGestureRecognizer _gestureRecognizer = TapGestureRecognizer()
+    ..onTap = () {
+      if (kDebugMode) {
+        print("Hello world from _gestureRecognizer");
+      }
+    };
+
+  bool fetchCredentials(String username, String password) {
+    for (var j = 0; j < _username.length; j++) {
+      if (username == _username[j] && password == _password[j]) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @override
@@ -193,8 +210,8 @@ class _LoginScreenState extends State<LoginScreen> {
                       print(
                           "Username: ${usernameController.text}, password: ${passwordController.text}");
                     }
-                    if (usernameController.text == fetchCredentials()[0] &&
-                        passwordController.text == fetchCredentials()[1]) {
+                    if (fetchCredentials(
+                        usernameController.text, passwordController.text)) {
                       Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(builder: (context) => HomePage()),
@@ -207,6 +224,34 @@ class _LoginScreenState extends State<LoginScreen> {
                     }
                   }),
             ),
+
+            // Register
+            Container(
+                padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: Center(
+                    child: RichText(
+                  text: TextSpan(
+                    text: "Dont have an account? ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                    children: [
+                      TextSpan(
+                          text: " Register here",
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold),
+                          recognizer: new TapGestureRecognizer()
+                            ..onTap = () => {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SignupPage()),
+                                  )
+                                }),
+                    ],
+                  ),
+                )))
           ],
         ));
   }
@@ -259,5 +304,261 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ));
+  }
+}
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SignupPage();
+}
+
+class _SignupPage extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: const Color.fromRGBO(40, 38, 56, 1),
+      body: SignupPageContent(),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.transparent,
+          elevation: 0,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Apple Developers' Group, PESU",
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          )),
+    );
+  }
+}
+
+class SignupPageContent extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SignupPageContent();
+}
+
+class _SignupPageContent extends State<SignupPageContent> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController1 = TextEditingController();
+  TextEditingController passwordController2 = TextEditingController();
+  bool _isVisible = false;
+  bool _isObscure1 = true;
+  bool _isObscure2 = true;
+  String returnVisibilityString = "";
+
+  bool isPasswordCompliant(String password, [int minLength = 6]) {
+    if (password == null || password.isEmpty) {
+      return false;
+    }
+
+    bool hasUppercase = password.contains(new RegExp(r'[A-Z]'));
+    bool hasDigits = password.contains(new RegExp(r'[0-9]'));
+    bool hasLowercase = password.contains(new RegExp(r'[a-z]'));
+    bool hasSpecialCharacters =
+        password.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+    bool hasMinLength = password.length > minLength;
+
+    return hasDigits &
+        hasUppercase &
+        hasLowercase &
+        hasSpecialCharacters &
+        hasMinLength;
+  }
+
+  bool returnVisibility(String password1, String password2, String username) {
+    if (password1 != password2) {
+      returnVisibilityString = "Passwords do not match";
+    } else if (username == "") {
+      returnVisibilityString = "username cannot be empty";
+    } else if (password1 == "" || password2 == "") {
+      returnVisibilityString = "Password fields cant be empty";
+    } else if (!isPasswordCompliant(password1)) {
+      returnVisibilityString = "Not password compliant";
+    }
+    return true;
+  }
+
+  void signup(username, password) {
+    _username.add(usernameController.text);
+    _password.add(passwordController1.text);
+    _i++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      reverse: true,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // Signup Text
+          Center(
+            child: Container(
+              height: 320,
+              width: 400,
+              alignment: Alignment.center,
+              child: Text(
+                "Signup",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+
+          // Wrong password text
+          Visibility(
+            visible: _isVisible,
+            maintainSize: true,
+            maintainAnimation: true,
+            maintainState: true,
+            child: Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                returnVisibilityString,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 10,
+                ),
+              ),
+            ),
+          ),
+
+          // Signup Info
+          Container(
+            height: 215,
+            width: 530,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.white),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  onTap: () {
+                    setState(() {
+                      _isVisible = false;
+                    });
+                  },
+                  controller: usernameController, // Controller for Username
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Username",
+                      contentPadding: EdgeInsets.all(20)),
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                Divider(
+                  thickness: 3,
+                ),
+                TextFormField(
+                  onTap: () {
+                    setState(() {
+                      _isVisible = false;
+                    });
+                  },
+
+                  controller: passwordController1, // Controller for Password
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Password",
+                      contentPadding: EdgeInsets.all(20),
+                      // Adding the visibility icon to toggle visibility of the password field
+                      suffixIcon: IconButton(
+                        icon: Icon(_isObscure1
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure1 = !_isObscure1;
+                          });
+                        },
+                      )),
+                  obscureText: _isObscure1,
+                ),
+                Divider(
+                  thickness: 3,
+                ),
+                TextFormField(
+                  onTap: () {
+                    setState(() {
+                      _isVisible = false;
+                    });
+                  },
+
+                  controller: passwordController2, // Controller for Password
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Re-enter Password",
+                      contentPadding: EdgeInsets.all(20),
+                      // Adding the visibility icon to toggle visibility of the password field
+                      suffixIcon: IconButton(
+                        icon: Icon(_isObscure2
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure2 = !_isObscure2;
+                          });
+                        },
+                      )),
+                  obscureText: _isObscure2,
+                ),
+              ],
+            ),
+          ),
+
+          // Signup Submit button
+          Container(
+            width: 570,
+            height: 70,
+            padding: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+                color: Colors.pink,
+                child: Text("Submit", style: TextStyle(color: Colors.white)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                onPressed: () {
+                  if (kDebugMode) {
+                    print(
+                        "Username: ${usernameController.text}\npassword: ${passwordController1.text}\nretry password ${passwordController2.text}");
+                  }
+                  if (passwordController1.text == passwordController2.text &&
+                      passwordController2.text != "" &&
+                      isPasswordCompliant(passwordController1.text) &&
+                      usernameController != "") {
+                    if (kDebugMode) {
+                      print("Es");
+                    }
+                    signup(usernameController.text, passwordController1.text);
+                    if (kDebugMode) {
+                      print(
+                          "username: ${_username[_i - 1]}, password: ${_password[_i - 1]}");
+                    }
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                      (Route<dynamic> route) => false,
+                    );
+                  } else {
+                    setState(() {
+                      _isVisible = returnVisibility(passwordController1.text,
+                          passwordController2.text, usernameController.text);
+                    });
+                    if (kDebugMode) {
+                      print("not es");
+                    }
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
